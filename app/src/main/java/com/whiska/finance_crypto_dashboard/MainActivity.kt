ccd02697b7ac911e19f5327efa3877ea7c3a3f97
whiskahsieh@gmail.com <whiskahsieh@gmail.com>
2024-04-16 11:13:01 +0800

    first commit

---
+++ app/src/main/java/com/whiska/finance_crypto_dashboard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.whiska.finance_crypto_dashboard
+
+import android.os.Bundle
+
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "finance_crypto_dashboard"
+include(":app")
+ 
